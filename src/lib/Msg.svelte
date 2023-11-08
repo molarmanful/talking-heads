1cfc3f0481e3a88ba5900232c02923c695b3ff05
molarmanful <benjinpang@gmail.com>
2023-11-08 16:37:12 -0500

    mroe

--- src/lib/Msg.svelte
+++ src/lib/Msg.svelte
@@ -3,7 +3,7 @@
   export { clazz as class }
 </script>
 
-<div class="flex mt-3 {clazz}">
-  <div class="w-50 text-right pr-5"><slot name="h" /></div>
+<div class="flex mt-3 gap-8 {clazz}">
+  <div class="w-32 text-right"><slot name="h" /></div>
   <div class="flex-1"><slot name="b" /></div>
 </div>
