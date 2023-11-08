277380e565f5e5435c7a4aba4f2f94c8649e62c9
molarmanful <benjinpang@gmail.com>
2023-11-08 16:42:16 -0500

    better

--- src/lib/Msg.svelte
+++ src/lib/Msg.svelte
@@ -3,7 +3,7 @@
   export { clazz as class }
 </script>
 
-<div class="flex mt-3 gap-8 {clazz}">
-  <div class="w-32 text-right"><slot name="h" /></div>
-  <div class="flex-1"><slot name="b" /></div>
+<div class="flex mt-3 {clazz}">
+  <div class="w-36 text-right"><slot name="h" /></div>
+  <div class="flex-1 pl-5"><slot name="b" /></div>
 </div>
